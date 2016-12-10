1df311edae11505d9e32b7d2be66a14efcbad801
Megaju <julien_malle@outlook.fr>
2016-12-10 19:13:53 +0100

    parse .json OK

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
 $(document).ready(function(){  
 
-// Assign handlers immediately after making the request,
-// and remember the jqxhr object for this request
+
 $('#next-chest').click(function(){
     // on demande la prochaine robe, prochain pull...
     getImages('chest');
 });
 
+
 function getImages(zone) {
-    var url = "http://localhost/hackathon-swap" + "/?" + zone;
-    var jqxhr = $.get(url, function () {
+    
+    
+    var dess = {};
+    
+    
+    var url = "http://localhost/hackathon-swap/test.json"/* + "/?" + zone*/;
+    
+    
+    $.get(url, function(){
      // alert("success");
-    }).done(function ( data ) {
-        // alert("reponse success=" + data);
+    }, "json").done(function ( data ) {
+        alert("reponse success=" + data);
         // exploiter la reponse
-        alert(data); 
+        dress = /*JSON.parse*/(data);
+        for (var i = 0; i < 5; i++) {
+            console.log(dress[i].name);
+        }
     }).fail(function () {
         alert("error");
     }).always(function () {
         alert("finished");
     });
-    // Perform other work here ...
-    // Set another completion function for the request above
-    jqxhr.always(function () {
-        alert("second finished");
-    });
+    
+    
 }
+    
+    
 });
