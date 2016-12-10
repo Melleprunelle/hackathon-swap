7e061a521e3d3fe83e341a65b0a5a0796279ca40
Megaju <julien_malle@outlook.fr>
2016-12-10 18:49:00 +0100

    ajout req ajax

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
+$(document).ready(function(){  
+
 // Assign handlers immediately after making the request,
 // and remember the jqxhr object for this request
-var jqxhr = $.get( "example.php", function() {
-  alert( "success" );
-})
-  .done(function() {
-    alert( "second success" );
-  })
-  .fail(function() {
-    alert( "error" );
-  })
-  .always(function() {
-    alert( "finished" );
-  });
- 
-// Perform other work here ...
- 
-// Set another completion function for the request above
-jqxhr.always(function() {
-  alert( "second finished" );
+$('#next-chest').click(function(){
+    // on demande la prochaine robe, prochain pull...
+    getImages('chest');
+});
+
+function getImages(zone) {
+    var url = "http://localhost/hackathon-swap" + "/?" + zone;
+    var jqxhr = $.get(url, function () {
+     // alert("success");
+    }).done(function ( data ) {
+        // alert("reponse success=" + data);
+        // exploiter la reponse
+        alert(data); 
+    }).fail(function () {
+        alert("error");
+    }).always(function () {
+        alert("finished");
+    });
+    // Perform other work here ...
+    // Set another completion function for the request above
+    jqxhr.always(function () {
+        alert("second finished");
+    });
+}
 });
