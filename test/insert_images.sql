f2ca8721c739ff58da1787c0af88e11b663c0434
Juliette3529 <juliette@saint-jalme.fr>
2016-12-11 16:42:51 +0100

    routage/flight

--- test/insert_images.sql
+++ test/insert_images.sql
@@ -2,18 +2,18 @@
 -- Script SQL mise en place du jeu de test images
 -- Hakathon, 11/12/2016, Groupe "Tendances et modes"
 -- 
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( '1139455.129.png', 2, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( '1139469.129.png', 2, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( '1151290.129.png', 4, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( '1151854.129.png', 2, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'botines.png', 5, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'chemise1.png', 2, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'chemise2.png', 2, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'chemiseAfreuse.png', 2, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'decol1.png', 2, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'pentalon1.png', 3, 1);
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'pentalon2.png', 3, 1);    
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'potes.png', 5, 1);    
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'pull.png', 2, 1);    
-insert into clothes (link, insert into clothes (link, position_id, categories_id) values ( 'testhat.jpg', 1, 1);
+ insert into clothes (link, position_id, categories_id) values ( '1139455.129.png', 2, 1);
+ insert into clothes (link, position_id, categories_id) values ( '1139469.129.png', 2, 1);
+ insert into clothes (link, position_id, categories_id) values ( '1151290.129.png', 4, 1);
+ insert into clothes (link, position_id, categories_id) values ( '1151854.129.png', 2, 1);
+ insert into clothes (link, position_id, categories_id) values ( 'botines.png', 5, 1);
+ insert into clothes (link, position_id, categories_id) values ( 'chemise1.png', 2, 1);
+ insert into clothes (link, position_id, categories_id) values ( 'chemise2.png', 2, 1);
+ insert into clothes (link, position_id, categories_id) values ( 'chemiseAfreuse.png', 2, 1);
+ insert into clothes (link, position_id, categories_id) values ( 'decol1.png', 2, 1);
+ insert into clothes (link, position_id, categories_id) values ( 'pentalon1.png', 3, 1);
+ insert into clothes (link, position_id, categories_id) values ( 'pentalon2.png', 3, 1);    
+ insert into clothes (link, position_id, categories_id) values ( 'potes.png', 5, 1);    
+ insert into clothes (link, position_id, categories_id) values ( 'pull.png', 2, 1);    
+ insert into clothes (link, position_id, categories_id) values ( 'testhat.jpg', 1, 1);
     
